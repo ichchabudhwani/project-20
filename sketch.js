757d97a267d99a3b0f8d49bb5bb1699e4d567191
ichchabudhwani <87148708+ichchabudhwani@users.noreply.github.com>
2021-08-15 20:19:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var Dog;
+function preload(){
+
+
+}
+
+function setup() {
+    createCanvas (500,500);
+    Dog = createSprite(200,200,200,50); 
+
+}
+
+function draw() {
+    background ("lightblue");
+
+
+}
